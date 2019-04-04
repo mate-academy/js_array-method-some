a5abf5fc4f3d9c78dbe0fbf5d946e9578fd7d486
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 15:40:52 +0300

    implemented task and tests

---
+++ src/arrayMethodSome.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Some
+ */
+function applyCustomSome() {
+  [].__proto__.some2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomSome;
---
+++ src/arrayMethodSome.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const applyCustomSome = require('./arrayMethodSome');
+
+const source = [0, 10, 20, 30];
+applyCustomSome();
+
+test('`some2` is added to [].__proto__', () => {
+  expect([].some2)
+    .toBeInstanceOf(Function);
+});
+
+test('for (item) => item > 10', () => {
+  expect(source.some2(x => x > 10))
+    .toBe(true);
+});
+
+test('for (item) => item > 100', () => {
+  expect(source.some2(x => x > 100))
+    .toBe(false);
+});
+
+test('false is returned for []', () => {
+  expect([].some2(x => true))
+    .toBe(false);
+});
+
+test('for (item, index) => index > 0', () => {
+  expect(source.some2((x, i) => i > 0))
+    .toBe(true);
+});
+
+test('(item, index, arr) => arr === source)', () => {
+  expect(source.some2((x, i, arr) => arr === source))
+    .toBe(true);
+});
+
+test('Source array is not changed', () => {
+  expect(source)
+    .toEqual([0, 10, 20, 30]);
+});
