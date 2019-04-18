ec0cba5c437f6eac39f634e20dc913d0ec47f874
VladimirTi <tytmes@mail.ru>
2019-04-18 21:46:08 +0300

    Vladimir`s_Slolution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -4,8 +4,13 @@
  * Implement method Some
  */
 function applyCustomSome() {
-  [].__proto__.some2 = function() {
-    // write code here
+  [].__proto__.some2 = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
