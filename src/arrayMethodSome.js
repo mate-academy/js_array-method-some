89ea35019a901ed32ca61762289ddaf919fd892f
Пользователь <Sviatoslv>
2019-06-21 19:13:34 +0300

    Solution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,7 +5,19 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    // write code here
+    const falseResult = false;
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+      const index = i;
+      const arr = this;
+
+      if (callback(item, index, arr)) {
+        return true;
+      }
+    }
+
+    return falseResult;
   };
 }
 
