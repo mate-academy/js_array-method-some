bf93903cbbbc7bacda4b0150af38a5bd7b98a858
DimaKashyrin <kashyrindima@gmail.com>
2019-06-24 04:39:20 +0300

    solution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,7 +5,16 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    // write code here
+    const arr = this;
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+      const index = i;
+
+      if (callback(item, index, arr)) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
