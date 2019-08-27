1fbe136e3c895568dcd1f8a8ef211fad9586f309
igaryok <igor.sobko@gmail.com>
2019-08-27 11:52:56 +0300

    delete variable

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,16 +5,15 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    let resultSome = false;
     let i = 0;
-    while (i < this.length && !resultSome) {
+    while (i < this.length) {
       if (callback(this[i], i, this)) {
-        resultSome = true;
+        return true;
       }
       i++;
     }
 
-    return resultSome;
+    return false;
   };
 }
 
