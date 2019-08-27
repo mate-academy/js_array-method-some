feafa0b79bf3ecefd221da3ad484d94f13e507cd
Miroslava <777shadowpriest777@gmail.com>
2019-08-27 13:32:24 +0300

    refactored

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,20 +5,13 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    if (this.length === 0) {
-      return false;
-    }
-
-    let result = false;
-
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        result = true;
-        break;
+        return true;
       }
     }
 
-    return result;
+    return false;
   };
 }
 
