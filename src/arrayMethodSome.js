aa96c6f185a7ef996ede00ae32a3d28c60bd976d
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-27 23:53:34 +0300

    'Fixed'

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -8,8 +8,9 @@ function applyCustomSome() {
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
         return true;
-      }
-    }
+      };
+    };
+
     return false;
   };
 }
