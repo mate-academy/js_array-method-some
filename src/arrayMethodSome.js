3d2c14b87cda58f8b428437842b13c013bd32fe3
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-27 18:13:28 +0300

    fix

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,14 +5,13 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    let result = false;
     for (let i = 0; i < this.length; i += 1) {
       if (callback(this[i], i, this)) {
-        result = true;
+        return true;
       }
     }
 
-    return result;
+    return false;
   };
 }
 
