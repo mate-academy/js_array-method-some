5d4b53fa9f35ee6085cbcf8c93e44bed37de5c82
Leon <FeelineKing@gmail.com>
2019-10-07 15:28:10 +0300

    Solution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -4,8 +4,13 @@
  * Implement method Some
  */
 function applyCustomSome() {
-  [].__proto__.some2 = function(callback) {
-    // write code here
+  Array.prototype.some2 = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this) === true) {
+        return true;
+      }
+    };
+    return false;
   };
 }
 
