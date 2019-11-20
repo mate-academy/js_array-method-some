54c0a15c60da95aa9be6a788b3943f91e784e00b
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-20 20:27:53 +0200

    Aidinov Alexii

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,7 +5,15 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    // write code here
+    let count = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        count++;
+      }
+    }
+
+    return count > 0;
   };
 }
 
