894401f52e90f49ad03b282132a9dee6f398a05b
natya2203 <nastya_2203@ukr.net>
2019-11-20 21:39:03 +0200

    solution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -5,7 +5,13 @@
  */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      const result = callback(this[i], i, this);
+      if (result) {
+        return true;
+      }
+    }
+    return false;
   };
 }
 
