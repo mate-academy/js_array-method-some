3495a12b4a0922e23ed7ecad6e9fd672a95a854e
VlaDinter <vladyslavdinter@gmail.com>
2020-05-31 03:26:14 +0300

    Solution

--- src/arrayMethodSome.js
+++ src/arrayMethodSome.js
@@ -1,11 +1,14 @@
 'use strict';
 
-/**
- * Implement method Some
- */
 function applyCustomSome() {
   [].__proto__.some2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return true;
+      }
+    }
+
+    return false;
   };
 }
 
